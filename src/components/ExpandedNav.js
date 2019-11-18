94b4049395ec86c1a765cad457e579ed45ee6c83
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-11-17 16:43:38 -0800

    Style expanded nav links

--- src/components/ExpandedNav.js
+++ src/components/ExpandedNav.js
@@ -7,6 +7,23 @@ class ExpandedNav extends Component {
     return (
       <div className="expanded-nav">
         <div className="expanded-nav__background"></div>
+        <div className="expanded-nav__links">
+          <a className="expanded-nav__link">
+            Home
+          </a>
+          <a className="expanded-nav__link">
+            Defi Accelerator
+          </a>
+          <a className="expanded-nav__link">
+            Compound Data
+          </a>
+          <a className="expanded-nav__link">
+            CSS Bundle
+          </a>
+          <a className="expanded-nav__link">
+            About Me
+          </a>
+        </div>
       </div>
     );
   }
