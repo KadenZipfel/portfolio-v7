fd5cd759d9c5a394de36225f738f342d2d7b5193
Kaden Zipfel <kaden.zipfel@hotmail.com>
2019-11-18 11:51:05 -0800

    Fade in project elements

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,15 +3,55 @@ import React, {Component} from 'react';
 import '../layout/components/project.sass';
 
 class Project extends Component {
+  componentDidMount = () => {
+    this.fadeInElements();
+  }
+  
+  fadeInElements = () => {
+    const wrapper = document.querySelector('.outer-wrapper');
+    const headers = document.querySelectorAll('.project__heading');
+    const images = document.querySelectorAll('.project__image');
+    const imageBackgrounds = 
+      document.querySelectorAll('.project__image-background');
+
+    wrapper.onscroll = () => {
+      const width = window.innerWidth;
+      const range = (width / 4) * 3;
+
+      headers.forEach((header) => {
+        const left = header.getBoundingClientRect().left;
+        if(left < range) {
+          header.classList.add('active')
+        }
+      });
+
+      images.forEach((image) => {
+        const left = image.getBoundingClientRect().left;
+        if(left < range) {
+          image.classList.add('active')
+        }
+      });
+
+      imageBackgrounds.forEach((background) => {
+        const left = background.getBoundingClientRect().left;
+        if(left < range) {
+          background.classList.add('active')
+        }
+      });
+    }
+  } 
+
   render() {
     return (
       <section className="project" id={this.props.id}>
-        <h2 className="project__header">
-          {this.props.header}
-        </h2>
-        <p className="project__description">
-          {this.props.description}
-        </p>
+        <div className="project__heading">
+          <h2 className="project__header">
+            {this.props.header}
+          </h2>
+          <p className="project__description">
+            {this.props.description}
+          </p>
+        </div>
         {/* Change this to an image */}
         <div className="project__image"></div>
         <div className="project__image-background"></div>
