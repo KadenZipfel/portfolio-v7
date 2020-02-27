0fcbb546ca3a262744ed57e0b36103ebba979fa5
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-02-27 10:41:48 -0800

    Update about me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -16,34 +16,30 @@ class About extends Component {
           </h2>
           <hr className="about__hr" />
           <p className="about__paragraph">
-            I have experience with many popular front end frameworks,
-            e.g. Angular, Vue, React - but my bread and butter is
-            React.
+            I am a front-end developer specializing in React, with 
+            experience building full-stack applications and marketing 
+            pages. With my background in design, I have an eye for 
+            detail that allows me to continuously perfect my work.
           </p>
           <p className="about__paragraph">
-            The only thing I like more than Javascript is Typescript.
+            My passion for open source software has given me experience 
+            in a large array of diverse programming environments, whilst 
+            greatly refining my knowledge of Git. By working on a large 
+            variety of projects, I have become comfortable efficiently 
+            learning new programming frameworks and paradigms.
           </p>
           <p className="about__paragraph">
-            Of course I’m very comfortable with Git and Sass.
+            Along my ongoing search for knowledge, I have learned the 
+            importance of several programming paradigms including 
+            functional vs. object oriented programming and error 
+            driven vs. test driven development. I have also built 
+            confidence in several languages and frameworks such as: 
+            React, Typescript, Node, Express and Sass.
           </p>
           <p className="about__paragraph">
-            I can write tests in Mocha, Chai, Jest, or whatever else 
-            you throw at me.
-          </p>
-          <p className="about__paragraph">            
-            I have experience working on small client marketing 
-            pages, full-stack applications, and a few big budget 
-            marketing pages
-          </p>
-          <p className="about__paragraph">
-            I also love to contribute to projects that excite me, 
-            either by meticulously searching for problems, or 
-            suggesting improvements. 
-          </p>
-          <p className="about__paragraph">
-            I like to spend my free time running, weightlifting, 
-            practicing jiu jitsu, working on personal projects, or 
-            hanging out with my girlfriend.
+            I am currently in search of a full-time position where I 
+            have the opportunity to work in a collaborative environment 
+            where I can continuously grow as both a developer and a person.
           </p>
         </div>
       </section>
