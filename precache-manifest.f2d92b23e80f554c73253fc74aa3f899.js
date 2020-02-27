3afb3f5f8c916eeaefccc3d28c490fe64be6a4a5
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-02-27 11:36:15 -0800

    Updates

---
+++ precache-manifest.f2d92b23e80f554c73253fc74aa3f899.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bdeb5cfcf68a10962d5bd063bab72fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6302a7d15cd3e42527fc",
+    "url": "/static/css/main.d3a0187b.chunk.css"
+  },
+  {
+    "revision": "d1e0ed5d995f5acc20de",
+    "url": "/static/js/2.730abb1d.chunk.js"
+  },
+  {
+    "revision": "6302a7d15cd3e42527fc",
+    "url": "/static/js/main.77d17a0d.chunk.js"
+  },
+  {
+    "revision": "47415b3cf21e37825a1e",
+    "url": "/static/js/runtime-main.dfbb36bd.js"
+  },
+  {
+    "revision": "d84e9d17314284490dafb6071e3014df",
+    "url": "/static/media/compound-data.d84e9d17.png"
+  },
+  {
+    "revision": "2eeff055645eb6f2a0d5c637359bf352",
+    "url": "/static/media/defi-accelerator.2eeff055.png"
+  },
+  {
+    "revision": "c352dbea72df20c128d5e102c3abdf2d",
+    "url": "/static/media/ethereum-landing.c352dbea.png"
+  }
+]);
