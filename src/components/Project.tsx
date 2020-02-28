d40de4f821b2863d9c20b9c28d2a12397a5257d8
Kaden Zipfel <kaden.zipfel@hotmail.com>
2020-02-27 16:29:42 -0800

    Tween project headers

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {ScrollScene} from 'scrollscene';
+import {TimelineMax} from 'gsap';
 
 import '../layout/components/project.sass';
 
@@ -12,24 +14,64 @@ interface ProjectProps {
 }
 
 class Project extends Component<ProjectProps> {
-  projectHeadings_: HTMLElement[];
+  private projectHeadings_: HTMLElement[];
+  private timeline0_: TimelineMax;
+  private timeline1_: TimelineMax;
+  private timeline2_: TimelineMax;
 
-  componentDidMount = () => {
+  componentDidMount = (): void => {
     this.projectHeadings_ = Array.from(document.querySelectorAll('.project__heading'));
+    this.timeline0_ = new TimelineMax({onUpdate: this.updatePercentage, paused: true});
+    this.timeline1_ = new TimelineMax({onUpdate: this.updatePercentage, paused: true});
+    this.timeline2_ = new TimelineMax({onUpdate: this.updatePercentage, paused: true});
 
-    this.fadeHeadings();
+    this.tweenHeadings();
   }
 
-  fadeHeadings = () => {
-    window.onscroll = () => {
-      this.projectHeadings_.forEach((heading) => {
-        if(heading.getBoundingClientRect().top < 170) {
-          heading.classList.add('active');
-        }
-      });
-    }
+  tweenHeadings = (): void => {
+    this.timeline0_.fromTo('.project__heading--0', 1, {x: 300, opacity: 0}, {x: 0, opacity: 1});
+
+    new ScrollScene({
+      triggerElement: '.project--0',
+      triggerHook: 'onLeave',
+      duration: '40%',
+      gsap: {
+        timeline: this.timeline0_
+      }
+    });
+
+    this.timeline1_.fromTo('.project__heading--1', 1, {x: 300, opacity: 0}, {x: 0, opacity: 1});
+
+    new ScrollScene({
+      triggerElement: '.project--1',
+      triggerHook: 'onLeave',
+      duration: '40%',
+      gsap: {
+        timeline: this.timeline1_
+      }
+    });
+
+    this.timeline2_.fromTo('.project__heading--2', 1, {x: 300, opacity: 0}, {x: 0, opacity: 1});
+
+    new ScrollScene({
+      triggerElement: '.project--2',
+      triggerHook: 'onLeave',
+      duration: '40%',
+      gsap: {
+        timeline: this.timeline2_
+      }
+    });
   }
 
+  updatePercentage = (): void => {
+    this.timeline0_.progress();
+    console.log(this.timeline0_.progress());
+    this.timeline1_.progress();
+    console.log(this.timeline1_.progress());
+    this.timeline2_.progress();
+    console.log(this.timeline2_.progress());
+  }
+  
   render() {
     return (
       <section 
